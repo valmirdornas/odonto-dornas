95cefc3df37043addba65f37770e2b97e06611f1
Valmir Dornas <fernandocursos2021@gmail.com>
2026-06-15 12:02:51 -0300

    Create tables

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/extension";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
